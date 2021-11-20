39848fb2ec1955a4081b0bf7483581e394e743c3
Radminas Usackas <usackasr@gmail.com>
2021-11-20 22:04:30 +0000

    Finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+function getDate() {
+  return new Date().toISOString().
+    replace(/T/, ' ').      // replace T with a space
+    replace(/\..+/, '')     // delete the dot and everything after
+}
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: getDate()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: getDate()
+  }
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Message Board', messages: messages });
 });
+
+router.post('/new', function (req, res, next) {
+  let newText = req.body.message;
+  let newUser = req.body.name;
+
+  if (newText.length > 0 && newUser.length > 0)
+
+    messages.push({
+      text: req.body.message,
+      user: req.body.name,
+      added: getDate()
+    });
+
+  res.redirect('/');
+})
 
 module.exports = router;
